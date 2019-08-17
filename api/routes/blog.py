45f45b8282999bf9a33779da9c02a0a1032a9776
Ole Schley <schley.ole@gmail.com>
2019-08-17 23:31:28 +0800

    initial commit

---
+++ api/routes/blog.py
@@ -0,0 +1,17 @@
+# new route
+# router.get('/new')
+
+# create route
+# router.post('/')
+
+# show route
+# router.get('/:id')
+
+# edit route
+# router.get('/:id/edit')
+
+# update route
+# router.put('/:id')
+
+# destroy route
+# router.delete('/:id')
