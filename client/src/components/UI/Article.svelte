041e7d67ef39e1c6ba390d8b62214c8177b4b2ce
Ole Schley <schley.ole@gmail.com>
2019-08-27 22:38:44 +0800

    remove unused

--- client/src/components/UI/Article.svelte
+++
@@ -1,5 +0,0 @@
-
-<article>
-    <p>Lorem ipsum dolor sit amet.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium blanditiis, rem, eius adipisci ipsa accusantium nesciunt dolore iusto asperiores aliquid ea reiciendis ab quae cumque quos aperiam dignissimos repudiandae rerum sit architecto id cupiditate dolores impedit laboriosam! Corrupti consequuntur eius maiores totam aut eveniet est exercitationem tempora illum obcaecati accusamus repudiandae eaque aliquam, perferendis veniam voluptate natus laboriosam. Expedita, animi dicta officia vero eligendi sapiente ad laborum autem voluptatibus numquam rerum a voluptates nostrum unde suscipit libero sint nihil, fugiat saepe ipsa totam excepturi recusandae! Ut ad dolorum, nemo neque distinctio sapiente repellendus? Nobis earum blanditiis, eos facere fugiat placeat.</p>
-</article>
