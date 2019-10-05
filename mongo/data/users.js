06b47c84e80578b00bec56fa1d086a20229c772d
Ole Schley <schley.ole@gmail.com>
2019-10-05 15:35:14 +0800

    await async functions

---
+++ mongo/data/users.js
@@ -0,0 +1,5 @@
+const firstNames = ["Amelia", "Margaret", "Emma", "Mary", "Samantha", "Olivia", "Patricia", "Isla", "Bethany", "Sophia", "Jennifer", "Isabella", "Elizabeth", "Joanne", "Linda", "Ava", "Megan", "Mia", "Barbara", "Victoria", "Emily", "Susan", "Jessica", "Lauren", "Abigail", "Lily", "Michelle", "Madison", "Sophie", "Tracy", "Charlotte", "Sarah", "Oliver", "Jake", "Noah", "James", "Jack", "Connor", "Liam", "John", "Harry", "Callum", "Mason", "Robert", "Jacob", "Kyle", "Thomas", "Joe", "Ethan", "David", "George", "Michael", "Richard", "Oscar", "Joseph", "Charles", "William", "Damian", "Daniel"]
+const lastNames = ["Bell", "Barnes", "Booth", "Butler", "Cooper", "Fisher", "Ford", "Johnson", "Holmes", "Miller", "Murray", "Simpson", "Smith", "Watson", "Wood"]
+const mailProviders = ["gmail.com", "yahoo.com", "hotmail.com", "gmx.com", "icloud.com"]
+
+module.exports = {firstNames, lastNames, mailProviders}
--- mongo/index.js
+++ mongo/index.js
@@ -1,53 +1,73 @@
+// External
 const _ = require('lodash')
-const fs = require('fs')
 const moment = require('moment')
 const mongoose = require('mongoose')
-const data = require('./data/base')
-// const async = require('async')
 
+// Internal
+const data = require('./data')
 const models = require('./models')
+
+// Mongo config
+mongoose.Promise = global.Promise
+mongoose.set('debug', true)
+
+const uri = 'mongodb://localhost:27017/fivetwentyfive'
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    user: 'testuser',
+    pass: 'testpass'
+}
 
 function generateDate(start, end) {
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 }
 
-// Generate user data
-let users = []
-for (let i = 0; i < 20; i++) {
-
-    let fname = _.sample(data.firstNames)
-    let lname = _.sample(data.lastNames)
-    let mail = _.sample(data.mailProviders)
-
-    let user = new models.User({
-        _id: mongoose.Types.ObjectId(),
-        name: fname,
-        email: `${fname.toLowerCase()}.${lname.toLowerCase()}@${mail}`
-    })
-
-    users.push(user)
-}
-
-
-// Generate posts
-let posts = []
-for (let i = 0; i < data.titles.length; i++) {
-    let created = generateDate(new Date(2016, 0, 1), new Date(2018, 0, 1))
-    let updated = generateDate(created, new Date())
-
-    let body = ''
-    for (let i = 0; i < _.sample([1, 2, 3, 4]); i++) {
-        body += _.sample(data.paragraphs)
+async function seedUsers() {
+    for (let i = 0; i < 20; i++) {
+        let fname = _.sample(data.Users.firstNames)
+        let lname = _.sample(data.Users.lastNames)
+        let mail = _.sample(data.Users.mailProviders)
+    
+        await models.User.create({
+            _id: mongoose.Types.ObjectId(),
+            name: fname,
+            email: `${fname.toLowerCase()}.${lname.toLowerCase()}@${mail}`
+        })
     }
-
-    let post = new models.Post({
-        _id: mongoose.Types.ObjectId(),
-        userId: _.sample(users)._id,
-        created: moment(created).toISOString(),
-        updated: moment(updated).toISOString(),
-        title: data.titles[i],
-        body: body
-    })
-
-    posts.push(post)
 }
+
+async function seedPosts() {
+    for (let i = 0; i < data.Posts.titles.length; i++) {
+        let created = generateDate(new Date(2016, 0, 1), new Date(2018, 0, 1))
+        let updated = generateDate(created, new Date())
+        let body = ''
+        for (let i = 0; i < _.sample([1, 2, 3, 4]); i++) {
+            body += _.sample(data.Posts.paragraphs)
+        }
+        let user = await models.User.aggregate().sample(1)
+
+        await models.Post.create({
+            _id: mongoose.Types.ObjectId(),
+            userId: user[0]._id,
+            created: moment(created).toISOString(),
+            updated: moment(updated).toISOString(),
+            title: data.Posts.titles[i],
+            body: body
+        })
+    }
+}
+
+// Seed database
+async function seedDatabase() {
+    await mongoose.connect(uri, options).catch(err => console.error(err))
+    await seedUsers()
+            .then(() => console.log('Successfully seeded users'))
+            .catch(err => console.error(err))
+    await seedPosts()
+            .then(() => console.log('Successfully seeded posts'))
+            .catch(err => console.error(err))
+    await mongoose.disconnect()
+}
+
+seedDatabase()
