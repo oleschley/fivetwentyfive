abaed2a9e98060e9fd335a28147cd01153ef4500
Ole Schley <schley.ole@gmail.com>
2019-10-06 20:01:25 +0800

    break up schema

--- api/__init__.py
+++ api/__init__.py
@@ -1,29 +1,14 @@
 import os
 
 from starlette.applications import Starlette
-
-from ariadne import (
-    load_schema_from_path,
-    make_executable_schema,
-)
+from ariadne import load_schema_from_path, make_executable_schema
 from ariadne.asgi import GraphQL
 
-from api.graphql.resolvers import (
-    blog,
-    users,
-    visuals
-)
+from api.graphql.resolvers import all_resolvers
 
-type_defs = load_schema_from_path('/api/graphql/')
-resolvers = [
-    blog.query,
-    blog.post,
-    users.query,
-    users.user,
-    visuals.query
-]
+type_defs = load_schema_from_path('/api/graphql/schema')
 
-schema = make_executable_schema(type_defs, resolvers)
+schema = make_executable_schema(type_defs, all_resolvers)
 
 app = Starlette(debug=True)
 app.mount('/graphql', GraphQL(schema, debug=True))
