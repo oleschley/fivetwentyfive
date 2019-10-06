6a4c1024b15a1c7d1d6e794ed2043239735d683e
Ole Schley <schley.ole@gmail.com>
2019-10-06 19:32:16 +0800

    remove greeting resolver

--- api/graphql/resolvers/greeting.py
+++
@@ -1,9 +0,0 @@
-from ariadne import QueryType
-
-query = QueryType()
-
-@query.field('greeting')
-def resolve_greeting(*_, name=None):
-    if name:
-        return f'Hello {name}'
-    return 'Helllloooo!'
