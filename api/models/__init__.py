6ae6b8dcd29036a32923095cb1419837cb73accc
Ole Schley <schley.ole@gmail.com>
2019-10-06 20:04:46 +0800

    remove models

--- api/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# From node for migration of MongoDB
-
-# if (process.env.NODE_ENV === 'production') {
-#     conn = `mongodb+srv://${process.env.FTF_USER}:${process.env.FTF_PWD}@${process.env.FTF_DB}?retryWrites=true`    
-# } else {
-#     conn = `mongodb://dev:dev@mongo:27017/fivetwentyfive?authSource=admin`
-# }
