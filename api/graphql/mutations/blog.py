8aa0e7d263ac8dd5f9de5f2823825d9176312916
Ole Schley <ole.schley@oldendorff.com>
2019-10-08 19:19:06 +0800

    add update and delete

--- api/graphql/mutations/blog.py
+++ api/graphql/mutations/blog.py
@@ -24,3 +24,35 @@ def create_post(obj, info, input):
         return {'status': 200, '_id': post_id}
     except:
         return {'status': 500, 'error': 'Ooops...'}
+
+@mutation.field('updatePost')
+def update_post(obj, info, input):
+
+    try: 
+        post_id = db.posts.find_one_and_update({
+            '_id': ObjectId(input['_id'])
+        },
+        {
+            '$set': {
+                'title': input['title'],
+                'body': input['body'],
+                'tags': input.get('tags'),
+                'published': input['published']
+            }
+        })
+
+        return {'status': 200, '_id': post_id}
+    except:
+        return {'status': 500, 'error': 'Ooops...'}
+
+@mutation.field('deletePost')
+def delete_post(obj, info, _id):
+    
+    try:
+        db.posts.find_one_and_delete({
+            '_id': ObjectId(_id)
+        })
+
+        return 200
+    except:
+        return {'status': 500, 'error': 'Ooops...'}
