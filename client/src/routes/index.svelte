3729cdb95f454cade24dc16d2fc1ea2afee0fc10
Ole Schley <schley.ole@gmail.com>
2019-10-27 23:38:13 +0800

    move splash page to index

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,7 +1,32 @@
-<style>
+<script>
+	import Logo from '../components/UI/Logo.svelte'
+</script>
+
+<svelte:head>
+	<title>DATA DESIGN NARUHODO</title>
+</svelte:head>
+
+<style lang="scss">
+
+    .splash {
+        height: 90vh;
+        padding-top: 4rem;
+        padding-left: 4rem;
+        display: grid;
+        grid-template-columns: repeat(9, 1fr);
+        grid-template-rows: repeat(6, 1fr);
+
+        grid-template-areas:
+            "logo logo logo . . . . . ."
+            "logo logo logo . . . . . ."
+            "logo logo logo . ch ch ch ch ."
+            " . . . . ch ch ch ch ."
+            " . . . . ch ch ch ch ."
+            " . . . . ch ch ch ch ."
+    }
 
 </style>
 
-<svelte:head>
-	<title>Landing Page</title>
-</svelte:head>
+<div class="splash">
+	<Logo/>
+</div>
