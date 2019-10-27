b5e857a732fedf0a25b8abaecdf281f0027406d3
Ole Schley <schley.ole@gmail.com>
2019-10-27 23:32:05 +0800

    define grid of landing page

---
+++ client/src/components/Landing/Index.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Logo from '../UI/Logo.svelte'
+</script>
+
+<style lang="scss">
+
+    .splash {
+        height: 90vh;
+        padding-top: 4rem;
+        padding-left: 4rem;
+        display: grid;
+        grid-template-columns: repeat(9, 1fr);
+        grid-template-rows: repeat(6, 1fr);
+
+        grid-template-areas:
+            "logo logo logo . . . . . ."
+            "logo logo logo . . . . . ."
+            "logo logo logo . ch ch ch ch ."
+            " . . . . ch ch ch ch ."
+            " . . . . ch ch ch ch ."
+            " . . . . ch ch ch ch ."
+    }
+
+</style>
+
+<div class="splash">
+    <Logo/>
+</div>
