cde25c0a90d8e23ef50acbbb31fb688e58c6a786
Ole Schley <schley.ole@gmail.com>
2019-10-27 23:38:30 +0800

    remove styling from layout

--- client/src/routes/_layout.svelte
+++ client/src/routes/_layout.svelte
@@ -1,31 +1,10 @@
 <!-- MAIN LAYOUT PAGE -->
 <script>
-	import Navbar from '../components/UI/Navbar.svelte';
+
 </script>
 
-<style>
 
-.wrapper {
-    width: 95%;
-    margin: 0 auto;
-    margin-top: 2vh;
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: 3rem 1fr 2rem;
-    grid-gap: 1rem;
-}
-
-footer {
-    color: var(--gray);
-}
-
-</style>
-
-<div class="wrapper">
-	<Navbar/>
-	<main>
-		<slot></slot>
-	</main>
-	<footer>俺はオレ</footer>
-</div>
+<main>
+    <slot></slot>
+</main>
 
